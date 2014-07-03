ba67d17e2dc8149aeb9c7652bcb7d6cd2132356d
Sam Marks <sam@sammarks.me>
2014-07-03 15:59:58 -0400

    Throw an exception if we cannot connect to etcd when instantating the GlobalKnowledge.

--- src/Able/Helpers/GlobalKnowledge/GlobalKnowledge.php
+++ src/Able/Helpers/GlobalKnowledge/GlobalKnowledge.php
@@ -2,6 +2,7 @@
 
 namespace Able\Helpers\GlobalKnowledge;
 
+use Guzzle\Common\Exception\ExceptionCollection;
 use LinkORB\Component\Etcd\Client;
 
 class GlobalKnowledge extends Client {
@@ -19,14 +20,37 @@ class GlobalKnowledge extends Client {
 	 * @param string $url The URL used to connect to etcd.
 	 *
 	 * @return GlobalKnowledge
+	 * @throws \Exception
 	 */
 	public static function getInstance($url = 'http://127.0.0.1:4001')
 	{
 		if (!self::$instance) {
 			self::$instance = new self($url);
+
+			// Make sure the server is alive.
+			if (!self::$instance->ping()) {
+				throw new \Exception('There was an error connecting to the etcd server at ' . $url);
+			}
 		}
 
 		return self::$instance;
+	}
+
+	/**
+	 * Ping
+	 *
+	 * Tests to see if the server is alive.
+	 *
+	 * @return bool
+	 */
+	public function ping()
+	{
+		try {
+			$this->get('/');
+			return true;
+		} catch (\Exception $ex) {
+			return false;
+		}
 	}
 
 }
