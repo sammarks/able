065cf725317bbac682305671b1bcab400af66390
Sam Marks <sam@sammarks.me>
2014-07-10 14:23:12 -0400

    Add documentation to the base database feature class.

--- src/Able/Helpers/Install/Features/Database/DatabaseFeature.php
+++ src/Able/Helpers/Install/Features/Database/DatabaseFeature.php
@@ -6,13 +6,50 @@ use Able\Helpers\Install\Features\Feature;
 
 abstract class DatabaseFeature extends Feature {
 
+	/**
+	 * The hostname.
+	 * @var string
+	 */
 	protected $host;
+
+	/**
+	 * The username.
+	 * @var string
+	 */
 	protected $username;
+
+	/**
+	 * The password.
+	 * @var string
+	 */
 	protected $password;
+
+	/**
+	 * The name of the database.
+	 * @var string
+	 */
 	protected $database;
+
+	/**
+	 * Whether or not to automatically create the database.
+	 * @var bool
+	 */
 	protected $create = false;
 
+	/**
+	 * Get Connection String
+	 *
+	 * Gets the connection string for connecting to the database.
+	 *
+	 * @return string
+	 */
 	public abstract function getConnectionString();
+
+	/**
+	 * Create Database
+	 *
+	 * Creates the database based on the supplied credentials.
+	 */
 	public abstract function createDatabase();
 
 	public function preCopy($directory)
