6a706e43aabe57e41e10bd7794a0235a40708d52
Sam Marks <sam@sammarks.me>
2014-08-07 14:45:25 -0400

    Update how ini values are handled.

--- src/Able/Helpers/Install/ConfigurationManagers/IniConfigurationManager.php
+++ src/Able/Helpers/Install/ConfigurationManagers/IniConfigurationManager.php
@@ -61,24 +61,33 @@ abstract class IniConfigurationManager extends ConfigurationManager {
 				foreach ($elem as $key2 => $elem2) {
 					if (is_array($elem2)) {
 						for ($i = 0; $i < count($elem2); $i++) {
-							$content .= $key2 . "[] = \"" . $elem2[$i] . "\"\n";
+							$content .= $key2 . "[] = " . $this->iniValue($elem2[$i]) . "\n";
 						}
 					} else if ($elem2 == "") $content .= $key2 . " = \n";
-					else $content .= $key2 . " = \"" . $elem2 . "\"\n";
+					else $content .= $key2 . " = " . $this->iniValue($elem2) . "\n";
 				}
 			}
 		} else {
 			foreach ($assoc_arr as $key => $elem) {
 				if (is_array($elem)) {
 					for ($i = 0; $i < count($elem); $i++) {
-						$content .= $key . "[] = \"" . $elem[$i] . "\"\n";
+						$content .= $key . "[] = " . $this->iniValue($elem[$i]) . "\n";
 					}
 				} else if ($elem == "") $content .= $key . " = \n";
-				else $content .= $key . " = \"" . $elem . "\"\n";
+				else $content .= $key . " = " . $this->iniValue($elem) . "\n";
 			}
 		}
 
 		return $content;
+	}
+
+	protected function iniValue($value)
+	{
+		if (is_numeric($value)) {
+			return $value;
+		} else {
+			return "\"{$value}\"";
+		}
 	}
 
 }
