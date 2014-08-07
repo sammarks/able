16ffa9606b9bd38059a528c68f82f710bea58f69
Sam Marks <sam@sammarks.me>
2014-08-07 15:29:14 -0400

    Make sure nginx only runs in daemon mode.

---
+++ src/Able/Helpers/Install/ConfigurationManagers/NginxConfigurationManager.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Able\Helpers\Install\ConfigurationManagers;
+
+class NginxConfigurationManager extends FileConfigurationManager {
+
+	protected function getBaseFile()
+	{
+		return '/etc/nginx/nginx.conf';
+	}
+
+	protected function getBaseReplacementList()
+	{
+		return array(
+			'global.before',
+			'global.after',
+		);
+	}
+
+	public function postInitialize()
+	{
+		parent::postInitialize();
+
+		// Append the [ablecore:base:global:before] and [ablecore:base:global:after] to the file.
+		if ($contents = file_get_contents($this->base_file)) {
+
+			$contents = "[ablecore:base:global:before]\n\n" . $contents;
+			$contents .= "\n\n[ablecore:base:global:after]";
+
+			if (!file_put_contents($this->base_file, $contents)) {
+				throw new NginxConfigurationManagerException('There was an error saving the default nginx configuration.');
+			}
+
+		} else {
+			throw new NginxConfigurationManagerException('Could not open the default nginx configuration for modification.');
+		}
+	}
+
+}
+
+class NginxConfigurationManagerException extends \Exception {}
---
+++ src/Able/Helpers/Install/Features/NginxFeature.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Able\Helpers\Install\Features;
+
+class NginxFeature extends Feature {}
